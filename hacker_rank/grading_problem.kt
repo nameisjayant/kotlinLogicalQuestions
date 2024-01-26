5793bc5918acbfc7cfc0dde25c99af0832591471
nameisjayant <jayntkumar99@gmail.com>
2024-01-26 19:35:59 +0530

    solved grading student problem

--- hacker_rank/grading_problem.kt
+++ hacker_rank/grading_problem.kt
@@ -1,3 +1,34 @@
 package hacker_rank
 
 
+fun main() {
+    gradingStudent(arrayOf(73, 67, 38, 33))
+}
+
+fun gradingStudent(grades: Array<Int>) {
+
+    val resArray = arrayListOf<Int>()
+
+    grades.forEach {
+        if (it.lessThan38())
+            resArray.add(it)
+        else if (it.findMultipleOf5()) {
+            val divisible = it / 5
+            val compare = ((divisible + 1) * 5) - it
+            val roundOff = it + compare
+            resArray.add(roundOff)
+        } else {
+            resArray.add(it)
+        }
+    }
+    println(resArray.map { it })
+
+}
+
+fun Int.findMultipleOf5(): Boolean {
+    val divisible = this / 5
+    val compare = ((divisible + 1) * 5) - this
+    return compare < 3
+}
+
+fun Int.lessThan38() = this < 38
